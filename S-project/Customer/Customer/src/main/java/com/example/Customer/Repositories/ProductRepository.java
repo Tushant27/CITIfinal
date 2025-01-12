69acfb6b0aee00d41c3757726a8ce399297b4cdb
Tushant27 <tualoney27@gmail.com>
2025-01-12 22:47:11 +0530

    Delete S-project directory

--- S-project/Customer/Customer/src/main/java/com/example/Customer/Repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Customer.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Customer.pojo.Products;
-
-public interface ProductRepository extends JpaRepository<Products, Integer>{
-
-}
