af442f1a0b52093a4f6b3e5b38183bf23b27c28d
Tushant27 <tualoney27@gmail.com>
2025-02-11 10:19:32 +0530

    Delete Citi-Ecom-main directory

--- Citi-Ecom-main/Admin/src/main/java/com/example/Admin/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Admin.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Admin.pojos.Products;
-
-public interface ProductRepository extends JpaRepository<Products, Integer> {
-	
-}
--- Citi-Ecom-main/README.md
+++
@@ -1,7 +0,0 @@
-Steps to run -
-
-1) Import all 4 projects in STS as Maven Projects
-2) Run them all as Spring Boot App in order
-    - ServiceDiscovery
-    - Services(Admin/Customer)
-    - APIGateway
